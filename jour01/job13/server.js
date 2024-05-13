669df05932a1a819583d08625c5c7e10a0dd515a
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-05-13 14:16:44 +0200

    Improving codes

--- jour01/job13/server.js
+++ jour01/job13/server.js
@@ -1,31 +1,30 @@
 const fs = require('fs');
 const http = require('http');
 
+const goToPage = (filepath, response) => {
+  fs.readFile(filepath, 'utf8', (error, data) => {
+    if (error) {
+      response.writeHead(404);
+      response.end(error);
+    } else {
+      response.setHeader('Content-Type', 'text/html');
+      response.writeHead(200);
+      response.end(data);
+    }
+  });
+};
+
 const server = http.createServer((request, response) => {
   switch (request.url) {
     case '/':
-      fs.readFile('jour01/job13/index.html', 'utf8', (error, data) => {
-        if (error) {
-          response.writeHead(404);
-          response.end(error);
-        } else {
-          response.setHeader('Content-Type', 'text/html');
-          response.writeHead(200);
-          response.end(data);
-        }
-      });
+      goToPage('jour01/job13/index.html', response);
       break;
     case '/about':
-      fs.readFile('jour01/job13/about.html', 'utf8', (error, data) => {
-        if (error) {
-          response.writeHead(404);
-          response.end(error);
-        } else {
-          response.setHeader('Content-Type', 'text/html');
-          response.writeHead(200);
-          response.end(data);
-        }
-      });
+      goToPage('jour01/job13/about.html', response);
+      break;
+    default:
+      response.writeHead(404);
+      response.end();
       break;
   }
 });
--- jour01/job14/server.js
+++ jour01/job14/server.js
@@ -1,43 +1,29 @@
 const fs = require('fs');
 const http = require('http');
 
+const goToPage = (filepath, response) => {
+  fs.readFile(filepath, 'utf8', (error, data) => {
+    if (error) {
+      response.writeHead(404);
+      response.end(error);
+    } else {
+      response.setHeader('Content-Type', 'text/html');
+      response.writeHead(200);
+      response.end(data);
+    }
+  });
+};
+
 const server = http.createServer((request, response) => {
   switch (request.url) {
     case '/':
-      fs.readFile('jour01/job14/index.html', 'utf8', (error, data) => {
-        if (error) {
-          response.writeHead(404);
-          response.end(error);
-        } else {
-          response.setHeader('Content-Type', 'text/html');
-          response.writeHead(200);
-          response.end(data);
-        }
-      });
+      goToPage('jour01/job14/index.html', response);
       break;
     case '/about':
-      fs.readFile('jour01/job14/about.html', 'utf8', (error, data) => {
-        if (error) {
-          response.writeHead(404);
-          response.end(error);
-        } else {
-          response.setHeader('Content-Type', 'text/html');
-          response.writeHead(200);
-          response.end(data);
-        }
-      });
+      goToPage('jour01/job14/about.html', response);
       break;
     default:
-      fs.readFile('jour01/job14/error.html', 'utf8', (error, data) => {
-        if (error) {
-          response.writeHead(404);
-          response.end(error);
-        } else {
-          response.setHeader('Content-Type', 'text/html');
-          response.writeHead(200);
-          response.end(data);
-        }
-      });
+      goToPage('jour01/job14/error.html', response);
       break;
   }
 });
