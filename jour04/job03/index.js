2f3c9c99b96130b9a64fce56ddb8d1c40873dbda
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-05-16 20:25:20 +0200

    Adding Job03

---
+++ jour04/job03/index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@runtrackjscluster.ykdd4bt.mongodb.net/${process.env.DATABASE_NAME}`
+  )
+  .then(() => console.log('Connected to the database!'))
+  .catch((error) => console.error(error));
+
+// Creating schemas and models
+const studentSchema = new mongoose.Schema({
+  lastname: String,
+  firstname: String,
+  student_number: Number,
+  year_id: mongoose.Schema.Types.ObjectId,
+});
+
+const Student = mongoose.model('student', studentSchema, 'student');
+
+async function setupData() {
+  await new Student({
+    lastname: 'LeBricoleur',
+    firstname: 'Bob',
+    student_number: 1,
+    year_id: mongoose.Types.ObjectId.createFromHexString('664619ad96cf20fb30914dff'),
+  }).save();
+
+  await new Student({
+    lastname: 'Doe',
+    firstname: 'John',
+    student_number: 2,
+    year_id: mongoose.Types.ObjectId.createFromHexString('664619ad96cf20fb30914e04'),
+  }).save();
+
+  await new Student({
+    lastname: 'Dupont',
+    firstname: 'Marine',
+    student_number: 3,
+    year_id: mongoose.Types.ObjectId.createFromHexString('664619ad96cf20fb30914e06'),
+  }).save();
+}
+
+setupData();
