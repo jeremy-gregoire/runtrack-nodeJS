9c34d321ff3e7f55145a5361f182767cacd2fde5
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-05-16 21:07:38 +0200

    Changing naming convention

--- jour04/job04/index.js
+++ jour04/job04/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`
+    `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE_NAME}`
   )
   .then(() => console.log('Connected to the database!'))
   .catch((error) => console.error(error));
